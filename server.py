263c9887341d5f1db8bfc92a01f75a4afce45d83
bodyazj96 <85449935+bodyazj96@users.noreply.github.com>
2021-07-08 00:33:47 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import os
 import csv
 import glob
+import shutil
 from fpdf import FPDF
 from flask import Flask, render_template, request, redirect, flash, send_from_directory, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'C:/Users/bodya/Desktop/web server/uploaded_files'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = './uploaded_files'
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -17,36 +18,54 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+user_dir = os.path.join(app.config['UPLOAD_FOLDER'], 'new_folder')
+
 @app.route('/uploads/<name>')
 def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
 
 @app.route('/imgtopdf', methods=['GET', 'POST'])
 def upload_file():
-	if request.method == 'POST':
+    if request.method == 'POST':
 		# check if the post request has the file part
-		if 'files[]' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		files = request.files.getlist('files[]')
+        for file in glob.glob(f"{app.config['UPLOAD_FOLDER']}/*.*"):
+            os.remove(file)
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        files = request.files.getlist('files[]')
 		# If the user does not select a file, the browser submits an
 		# empty file without a filename.
 		# if files.filename == '':
 		# 	flash('No selected file')
 		# 	return redirect(request.url)
-		for file in files:
-			if file and allowed_file(file.filename):
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		pdf = FPDF()
-		imagelist=[]
-		for file in glob.glob('C:/Users/bodya/Desktop/web server/uploaded_files/*.png'):
-			imagelist.append(file)
-		for image in imagelist:
-			pdf.add_page()
-			pdf.image(image, w=190, h=150)
-		pdf.output("C:/Users/bodya/Desktop/web server/uploaded_files/your_file.pdf", "F")
-		return redirect(url_for('download_file', name='your_file.pdf'))
+        if not os.path.exists(user_dir):
+            os.mkdir(user_dir)
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(user_dir, filename))
+        pdf = FPDF()
+        imagelist=[]
+        pdfnamae = request.form['pdf_name']
+        for file in glob.glob(f'{user_dir}/*.*'):
+            imagelist.append(file)
+        for image in imagelist:
+            pdf.add_page()
+            option = request.form['position']
+            if option == 'center':
+                pdf.image(image, x=50, y=75, w=110, h=150)
+            elif option == 'top right corner':
+                pdf.image(image, x=100, y=0, w=110, h=150)
+            elif option == 'top left corner':
+                pdf.image(image, x=0, y=0, w=110, h=150)
+            elif option == 'bottom right corner':
+                pdf.image(image, x=100, y=150, w=110, h=150)
+            elif option == 'bottom left corner':
+                pdf.image(image, x=0, y=150, w=110, h=150)
+        pdf.output(f"{app.config['UPLOAD_FOLDER']}/{pdfnamae}.pdf", "F")
+        shutil.rmtree(user_dir)
+        return redirect(url_for('download_file', name=f'{pdfnamae}.pdf'))
 
 @app.route("/")
 def my_home():
