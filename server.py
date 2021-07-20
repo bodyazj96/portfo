c02b3ec81d57f3e1cc1d10c5096140257a211881
bodyazj96 <85449935+bodyazj96@users.noreply.github.com>
2021-07-20 11:03:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,10 @@ import csv
 import glob
 import shutil
 from fpdf import FPDF
+from PIL import Image, ImageFilter, ImageOps
 from flask import Flask, render_template, request, redirect, flash, send_from_directory, url_for
 from werkzeug.utils import secure_filename
+from flask_recaptcha import ReCaptcha
 
 UPLOAD_FOLDER = './uploaded_files'
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
@@ -12,7 +14,11 @@ ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
-app.secret_key = "2f7507b6ba103d240d9d7535dac60ea697be7ee2"
+app.secret_key = "APP_SECRET_KEY"
+app.config['RECAPTCHA_SITE_KEY'] = 'SITE_KEY'
+app.config['RECAPTCHA_SECRET_KEY'] = 'SECRET_KEY'
+recaptcha = ReCaptcha(app=app)
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -20,52 +26,94 @@ def allowed_file(filename):
 
 user_dir = os.path.join(app.config['UPLOAD_FOLDER'], 'new_folder')
 
-@app.route('/uploads/<name>')
-def download_file(name):
+@app.route('/pdf_with_images/<name>')
+def preview_pdf_with_images(name):
     return send_from_directory(app.config['UPLOAD_FOLDER'], name)
 
 @app.route('/imgtopdf', methods=['GET', 'POST'])
-def upload_file():
+def insert_images_to_pdf():
+	
+"""
+This function inserts images to a pdf it creates. One page - one image
+
+"""
+
     if request.method == 'POST':
-		# check if the post request has the file part
+        
+	# verifying reCAPTCHA:
+	
+        if recaptcha.verify():
+            message = 'Thanks for filling out the form!'
+        else:
+            message = 'Please fill out the ReCaptcha!'
+            flash(message, 'error')
+            return render_template('imgtopdf.html')
+
+        # removing a pdf file created by a previous user from upload folder:
+	
         for file in glob.glob(f"{app.config['UPLOAD_FOLDER']}/*.*"):
             os.remove(file)
-        if 'files[]' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+        
+	"""
+	uploading user's images; creating directory for user's files (if does not exist);
+	checking if user's files have allowed extensions; uploading files to user_dir:
+	
+	"""
+	
         files = request.files.getlist('files[]')
-		# If the user does not select a file, the browser submits an
-		# empty file without a filename.
-		# if files.filename == '':
-		# 	flash('No selected file')
-		# 	return redirect(request.url)
         if not os.path.exists(user_dir):
             os.mkdir(user_dir)
+
         for file in files:
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(user_dir, filename))
+        
+	"""
+	adjusting images' resolution for better efficiency; sharpening the images;
+        using EXIF code to position images correctly on page (according to their native orientation):
+	"""
+	
+        for item in glob.glob(f"{user_dir}/*.*"):
+            img = Image.open(item)
+            img.thumbnail((800, 820))
+            i = img.filter(ImageFilter.SHARPEN)
+            im = ImageOps.exif_transpose(i)
+            im.save(item)
+	
+	"""
+	inserting adjusted images to a pdf page by page;
+        user chooses image's position on page and the name of the pdf file which will be created: 
+	"""
+        
         pdf = FPDF()
         imagelist=[]
-        pdfnamae = request.form['pdf_name']
+        pdftitle = request.form['pdf_name']
+
         for file in glob.glob(f'{user_dir}/*.*'):
             imagelist.append(file)
         for image in imagelist:
             pdf.add_page()
             option = request.form['position']
             if option == 'center':
-                pdf.image(image, x=50, y=75, w=110, h=150)
+                pdf.image(image, x=50, y=80, w=110)
             elif option == 'top right corner':
-                pdf.image(image, x=100, y=0, w=110, h=150)
+                pdf.image(image, x=100, y=0, w=110)
             elif option == 'top left corner':
-                pdf.image(image, x=0, y=0, w=110, h=150)
+                pdf.image(image, x=0, y=0, w=110)
             elif option == 'bottom right corner':
-                pdf.image(image, x=100, y=150, w=110, h=150)
+                pdf.image(image, x=100, y=150, w=110)
             elif option == 'bottom left corner':
-                pdf.image(image, x=0, y=150, w=110, h=150)
-        pdf.output(f"{app.config['UPLOAD_FOLDER']}/{pdfnamae}.pdf", "F")
+                pdf.image(image, x=0, y=150, w=110)
+        pdf.output(f"{app.config['UPLOAD_FOLDER']}/{pdftitle}.pdf", "F")
+	
+	# removing user_dir containing user's images:
+	
         shutil.rmtree(user_dir)
-        return redirect(url_for('download_file', name=f'{pdfnamae}.pdf'))
+	
+	# redirecting user to the page containing PDF viewer; user can view their pdf file with images there:
+	
+        return redirect(url_for('preview_pdf_with_images', name=f'{pdftitle}.pdf'))
 
 @app.route("/")
 def my_home():
@@ -74,13 +122,6 @@ def my_home():
 @app.route("/<string:page_name>")
 def html_page(page_name):
 	return render_template(f'{page_name}')
-
-# @app.route("/file_ready", methods=['GET', 'POST'])
-# def imgtopdf():
-#     if request.method == 'POST':
-#         f = request.files['file']
-#         f.save()
-#         return redirect('download_file.html')
 
 def write_to_csv(data):
 	with open('database.csv', newline='', mode='a') as database:
@@ -92,14 +133,24 @@ def write_to_csv(data):
 		csv_writer=csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		csv_writer.writerow([email,subject,message])
 
-@app.route('/submit_form', methods=['POST', 'GET'])
-def submit_form():
-	if request.method == 'POST':
-		try:
-			data = request.form.to_dict()
-			write_to_csv(data)
-			return redirect('/thank_you.html')
-		except:
-			return "Did not save to database"
-	else:
-		return "Something went wrong. Try again."
+@app.route('/contact', methods=['POST', 'GET'])
+
+def contact_me():
+
+    if request.method == 'POST':
+
+        if recaptcha.verify():
+            message = 'Thanks for filling out the form!'
+        else:
+            message = 'Please fill out the ReCaptcha!'
+            flash(message, 'error')
+            return render_template('contact.html')
+
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thank_you.html')
+        except:
+            return "Did not save to database"
+    else:
+        return "Something went wrong. Try again."
